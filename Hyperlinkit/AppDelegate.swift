16cb3f32a08b04c543beeefcb5e145221f5e8e26
Alejandra Ledesma <Alejandra.Ledesma@nick.com>
2019-05-06 06:28:04 -0700

    Initial Commit

---
+++ Hyperlinkit/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Hyperlinkit
+//
+//  Created by Ledesma, Alejandra on 5/6/19.
+//  Copyright © 2019 Ledesma, Alejandra. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
